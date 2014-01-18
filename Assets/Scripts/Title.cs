6a5b0037db4456cf98825c9c56e86b424b86f279
Jon Wiedmann <jonwiedmann@gmail.com>
2014-01-18 00:21:22 -0500

    audio2

--- Assets/Scripts/Title.cs
+++ Assets/Scripts/Title.cs
@@ -6,14 +6,17 @@ public class Title : MonoBehaviour {
 	public Font font;
 
 	GUIStyle style = new GUIStyle();
+	GUIStyle bg = new GUIStyle();
 
 	void Start(){
-		//style.fontStyle;
+		style.normal.textColor = "#fff";
 		style.fontSize = 12;
-		style.font = font; 
+		style.font = font;
 	}
 
 	void OnGUI() {
+		GUI.color = "#000";
+		GUI.Box(new Rect(0, 0, 300, 300));
 		GUI.Label(new Rect(10, 10, 100, 20), "Working Title", style);
 	}
 }

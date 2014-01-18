04d221ab3e915b61573d987e4d6be361702ff64e
Isaiah Hines <IsaiahHines@gmail.com>
2014-01-18 01:49:06 -0500

    Added enemies and movement

---
+++ Assets/Scripts/Follow.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Targets {
+	public static IList<GameObject> objects = new List<GameObject>();
+}
+
+public class Follow : MonoBehaviour {
+	public float maxSpeed;
+
+	void Start(){
+
+	}
+
+	void Update () {
+		if (Targets.objects.Count == 0)
+			return;
+
+		GameObject closestObj = Targets.objects[0];
+		float closestDist = (Targets.objects [0].transform.position - transform.position).magnitude;
+
+		if(Targets.objects.Count > 1)
+		{
+			foreach(GameObject obj in Targets.objects)
+			{
+				Vector2 dir = obj.transform.position - transform.position;
+				float distance = dir.magnitude;
+
+				if(distance < closestDist)
+				{
+					closestObj = obj;
+					closestDist = distance;
+				}
+			}
+		}
+
+		Vector2 moveDir = closestObj.transform.position - transform.position;
+		rigidbody2D.velocity = moveDir.normalized * maxSpeed;
+
+	}
+}

035841497067310fc75d188a5a8a8f05e9ecc698
Jon Wiedmann <jonwiedmann@gmail.com>
2014-01-17 21:12:50 -0800

    audio

---
+++ Assets/Scripts/Title.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Title : MonoBehaviour {
+
+	public Font font;
+
+	GUIStyle style = new GUIStyle();
+
+	void Start(){
+		//style.fontStyle;
+		style.fontSize = 12;
+		style.font = font; 
+	}
+
+	void OnGUI() {
+		GUI.Label(new Rect(10, 10, 100, 20), "Working Title", style);
+	}
+}

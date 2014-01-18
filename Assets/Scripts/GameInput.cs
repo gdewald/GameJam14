02ee426f1762ae6b95b9b671a514ec3114d8e7de
Jon Wiedmann <jonwiedmann@gmail.com>
2014-01-17 20:44:00 -0800

    generic split and combine

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -2,27 +2,45 @@
 using System.Collections;
 
 public class GameInput : MonoBehaviour {
-	
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+	void Update(){
 		if(Input.GetKeyDown(KeyCode.Space)){
 			toggleSplit();
 		}
 	}
 
 	void toggleSplit(){
-		if (!isSplit) {
-			Game.cube[0].transform.Translate(-1f, 0, 0);
-			Game.cube[1] = Instantiate(Game.pfCube, new Vector2 (1f, 0), Quaternion.identity) as GameObject;
+		if (!Player.isSplit) {
+			Player.startPoint[0] = Player.entity[0].transform.position;
+			Player.startPoint[1] = Player.entity[0].transform.position;
+
+			//Vector3 pos0 = new Vector3(Player.entity[0].transform.position.x - 1, Player.entity[0].transform.position.y, 0);
+			//Vector3 pos1 = new Vector3(Player.entity[0].transform.position.x + 1, Player.entity[0].transform.position.y, 0);
+
+			Vector3 pos0 = new Vector3(Player.entity[0].transform.position.x - 3, Player.entity[0].transform.position.y - 4, 0);
+			Vector3 pos1 = new Vector3(Player.entity[0].transform.position.x + 2, Player.entity[0].transform.position.y + 1, 0);
+
+
+			Player.endPoint[0] = pos0;
+			Player.endPoint[1] = pos1;
+
+			Player.entity[1] = Instantiate(Game.pfCube, Player.entity[0].transform.position, Quaternion.identity) as GameObject;
+			Player.doSplit = true;
 		} 
 		else {
-			doCombine = true;
+			Player.startPoint[0] = Player.entity[0].transform.position;
+			Player.startPoint[1] = Player.entity[1].transform.position;
+
+			float x = Player.entity[0].transform.position.x + Player.entity[1].transform.position.x;
+			float y = Player.entity[0].transform.position.y + Player.entity[1].transform.position.y;
+
+			Vector3 pos = new Vector3(x/2, y/2, 0);
+						
+			Player.endPoint[0] = pos;
+			Player.endPoint[1] = pos;
+
+			Player.doCombine = true;
 		}
 		
-		isSplit = !isSplit;
+		Player.isSplit = !Player.isSplit;
 	}
 }

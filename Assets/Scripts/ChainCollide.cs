3a1ae3026e12b3d9908e89a5d0061369f792920b
Isaiah Hines <IsaiahHines@gmail.com>
2014-01-18 23:45:58 -0500

    Huge Merge - Player Behavior

--- Assets/Scripts/ChainCollide.cs
+++ Assets/Scripts/ChainCollide.cs
@@ -4,10 +4,6 @@ using System.Collections;
 public class ChainCollide : MonoBehaviour {
 	public float speed;
 
-	GameObject enemy = Resources.Load<GameObject>("Enemy");
-	GameObject firstEnemy;
-	GameObject secondEnemy;
-
 	void Update () {
 		Vector2 vel = transform.rigidbody2D.velocity;
 		if (vel.magnitude < speed) {
@@ -17,26 +13,9 @@ public class ChainCollide : MonoBehaviour {
 	
 	void OnTriggerEnter2D(Collider2D other){
 		GameObject obj = other.gameObject;
-		if (obj.tag == "SplitEnemy") {
-			Destroy (obj);
-			firstEnemy = Instantiate (enemy, obj.transform.position, Quaternion.identity) as GameObject;
-			secondEnemy = Instantiate (enemy, obj.transform.position, Quaternion.identity) as GameObject;
-			
-			Vector3 distanceVec = Player.entity[0].transform.position - Player.entity[1].transform.position;
-			Vector3 directionVec = Vector3.Cross(distanceVec, new Vector3(0, 0, 1));
-			
-			firstEnemy.GetComponent<Follow>().enabled = false;
-			secondEnemy.GetComponent<Follow>().enabled = false;
-			
-			firstEnemy.transform.rigidbody2D.velocity = directionVec;
-			secondEnemy.transform.rigidbody2D.velocity = -directionVec;
-			StartCoroutine(delayedExec());
+		if (obj.tag == "Enemy") {
+			Enemy enemy = other.GetComponent<Enemy>();
+			enemy.TakeChainHit(rigidbody2D.velocity, 0);
 		}
-	}
-
-	IEnumerator delayedExec(){
-		yield return new WaitForSeconds(0.5f);
-		firstEnemy.GetComponent<Follow>().enabled = true;
-		secondEnemy.GetComponent<Follow> ().enabled = true;
 	}
 }
--- Assets/Scripts/EnemyLarge.cs
+++ Assets/Scripts/EnemyLarge.cs
@@ -23,15 +23,27 @@ public class EnemyLarge : Enemy {
 			firstEnemy.GetComponent<Enemy>().SetHitsLeft(hitsLeft);
 			secondEnemy.GetComponent<Enemy>().SetHitsLeft(hitsLeft);
 
+			Vector3 distanceVec = Player.entity[0].transform.position - Player.entity[1].transform.position;
+			Vector3 directionVec = Vector3.Cross(distanceVec, new Vector3(0, 0, 1));
+
+
+			firstEnemy.transform.rigidbody2D.velocity = directionVec;
+			secondEnemy.transform.rigidbody2D.velocity = -directionVec;
+
+			firstEnemy.GetComponent<Follow>().SetDelay(0.5f);
+			secondEnemy.GetComponent<Follow>().SetDelay(0.5f);
+			
 			Destroy (gameObject);
 
-			firstEnemy.GetComponent<Follow>().enabled = false;
-			secondEnemy.GetComponent<Follow>().enabled = false;
-			//firstEnemy.transform.rigidbody2D.AddForce(new Vector2(-10000, -10000));
-			//print ("hit");
-			firstEnemy.transform.rigidbody2D.velocity = new Vector2(5, 0);
-			secondEnemy.transform.rigidbody2D.velocity = new Vector2(-5, 0);
-			StartCoroutine(delayedExec());
+			//firstEnemy.GetComponent<Follow>().enabled = false;
+			//secondEnemy.GetComponent<Follow>().enabled = false;
+
+			//Destroy (rigidbody2D);
+			//Destroy (collider2D);
+			//Destroy (renderer);
+
+			//StartCoroutine(delayedExec());
+
 			break;
 		}
 		// Die
@@ -47,9 +59,12 @@ public class EnemyLarge : Enemy {
 			gameObject.GetComponent<SpriteRenderer>().sprite = GameObject.Instantiate(Resources.Load<Sprite>("Images/Enemy" + (hitsLeft-1).ToString())) as Sprite;
 	}
 
+	/*
 	private IEnumerator delayedExec(){
 		yield return new WaitForSeconds(0.5f);
 		firstEnemy.GetComponent<Follow>().enabled = true;
 		secondEnemy.GetComponent<Follow>().enabled = true;
+		Destroy (gameObject);
 	}
+	*/
 }

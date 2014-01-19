99b83b706a59559effd87cb56efbf9bc5155a301
unknown <gdewald@ADDC-ATL-416.adsroot.itcs.umich.edu>
2014-01-18 20:51:38 -0500

    Damage! Powerups! Life!

--- Assets/Scripts/Powerup.cs
+++ Assets/Scripts/Powerup.cs
@@ -14,51 +14,80 @@ public class Powerup : MonoBehaviour {
 	void Update () {
 		timer -= Time.deltaTime;
 		if(timer <= 0) {
+			endPowerup();
 			this.enabled = false;	
-			switch(name) {
-			case "SpeedPowerup":
-				Debug.Log ("Speed powerup ran out");
-				
-				GetComponent<Controller>().maxSpeed = 10;
-				break;
-			case "ChainPowerup":
-				Debug.Log ("Chain powerup ran out");
-				
-				foreach(GameObject o in GameObject.FindGameObjectsWithTag("ChainDestroy")) {
-					o.tag = "ChainSplit";
-				}
-				break;
-				
-			default:
-				break;
-			}
 		}
 	}
 	
 	void OnCollisionEnter2D(Collision2D col) {
-		if(col.collider.tag != "Powerup") return;
-		switch(col.collider.name) {
-			case "SpeedPowerup":
-				Debug.Log ("Collided with speed powerup");
-				name = col.collider.name;
-				GetComponent<Controller>().maxSpeed = 15;
-				timer = 5.0f;
-				this.enabled = true;
-				Destroy(col.collider.gameObject);
-			break;
-			case "ChainPowerup":
-				Debug.Log ("Collided with chain powerup");
-				name = col.collider.name;
-				foreach(GameObject o in GameObject.FindGameObjectsWithTag("ChainSplit")) {
-					o.tag = "ChainDestroy";
-				}
-				timer = 5.0f;
-				this.enabled = true;
-				Destroy(col.collider.gameObject);
-			break;
-			default:
-			break;
+		if(col.collider.tag == "Powerup") {
+			
+			Debug.Log ("Collided with " + col.collider.name);
+			startPowerup (col.collider.name);
+			this.enabled = true;
+			Destroy(col.collider.gameObject);
+		}
+		else if (col.collider.tag == "Resource") {
+			switch(col.collider.name) {
+				case "Health":
+					Player.life++;
+					Destroy(col.collider.gameObject);
+					break;
+				default:
+					break;
+			}
 		}
 		return;
+	}
+	
+	void startPowerup(string name_new) {
+		//stop current powerup
+		if(this.enabled) endPowerup();
+	
+		name = name_new;
+		
+		switch(name) {
+		case "SpeedPowerup":
+			GetComponent<Controller>().maxSpeed = 15;
+			timer = 10.0f;
+			break;
+		case "ChainPowerup":
+			foreach(GameObject o in GameObject.FindGameObjectsWithTag("ChainSplit")) {
+				o.tag = "ChainDestroy";
+			}
+			timer = 10.0f;
+			break;
+		case "SprayPowerup":
+			Player.entity[0].GetComponent<Controller>().fireMode = Controller.FireMode.SPRAY;
+			timer = 10.0f;
+			break;
+			
+		default:
+			break;
+		}
+	}
+	
+	void endPowerup() {
+		switch(name) {
+		case "SpeedPowerup":
+			Debug.Log ("Speed powerup ran out");
+			
+			GetComponent<Controller>().maxSpeed = 10;
+			break;
+		case "ChainPowerup":
+			Debug.Log ("Chain powerup ran out");
+			
+			foreach(GameObject o in GameObject.FindGameObjectsWithTag("ChainDestroy")) {
+				o.tag = "ChainSplit";
+			}
+			break;
+		case "SprayPowerup":
+			Debug.Log ("Spray powerup ran out");
+			
+			Player.entity[0].GetComponent<Controller>().fireMode = Controller.FireMode.SINGLE;
+			break;
+		default:
+			break;
+		}
 	}
 }

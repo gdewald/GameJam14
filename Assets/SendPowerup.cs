3cd372916bf2e73ec92d0783ac6b68f1899c4336
unknown <gdewald@ADDC-ATL-416.adsroot.itcs.umich.edu>
2014-01-19 14:47:04 -0500

    Powerups are triggers, modified powerup drops, tweaked powerups

---
+++ Assets/SendPowerup.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SendPowerup : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if(col.tag == "Target") {
+			col.gameObject.GetComponent("Powerup").SendMessage("RecvPowerup", this.gameObject);
+		}
+	}
+}

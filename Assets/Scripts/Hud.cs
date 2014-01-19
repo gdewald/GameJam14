d4190a56cb78a515ceb457e58887d4459acd67ff
Nicholas Dedenbach <ndedenbach@hotmail.com>
2014-01-19 18:36:54 -0500

    I don't remember.

--- Assets/Scripts/Hud.cs
+++ Assets/Scripts/Hud.cs
@@ -16,26 +16,15 @@ public class Hud : MonoBehaviour {
 	}
 	
 	void OnGUI() {
-	
-	
-		string num;// = ((Game.numKilled < 10) ? "0" : "") + Game.numKilled;
+		string num = ((Game.numKilled < 10) ? "0" : "") + Game.numKilled;
 
-		if(Game.numKilled < 100) {
-			num = "00";
-		}
-		else if(Game.numKilled < 10){
-			num = "0";
-		}
-		else num="";
-		num += Game.numKilled;
 		style.alignment = TextAnchor.UpperLeft;
 		style.fontSize = 11;
 
-		GUI.Label(new Rect(Screen.width - 114, 5, 100, 100), GameClock.strTime, style);
-		
-		GUI.Label(new Rect(Screen.width - 168, 21, 100, 100), "# Killed: " + num, style);
-		GUI.Label(new Rect(Screen.width - 135, 37, 100, 100), "Round:   " + GameLogic.roundNumber, style);
-		GUI.Label(new Rect(Screen.width - 124, 53, 100, 100), "Wave:   " + GameLogic.waveNumber, style);
+		GUI.Label (new Rect (Screen.width - 94, 5, 100, 100), GameClock.strTime, style);
+		GUI.Label (new Rect (Screen.width - 138, 21, 100, 100), "# Killed: " + num, style);
+		GUI.Label (new Rect (Screen.width - 105, 37, 100, 100), "Round:  " + GameLogic.roundNumber, style);
+		GUI.Label (new Rect (Screen.width - 94, 53, 100, 100), "Wave:  " + GameLogic.waveNumber, style);
 		GUI.Label (new Rect (0, 5, 100, 100), "Lives: " + Player.life, style);
 
 		if (Time.timeScale == 0.0f) {
@@ -43,9 +32,9 @@ public class Hud : MonoBehaviour {
 			style.alignment = TextAnchor.MiddleCenter;
 			GUI.Label (new Rect ((Screen.width * 0.5f) - 75, (Screen.height * 0.5f - 50), 150, 100), "GAME OVER", style); 
 			style.fontSize = 22;
-			GUI.Label (new Rect((Screen.width  * 0.5f) - 75, (Screen.height * 0.5f + 100), 150, 50), "Press SPACE for menu", style);
+			GUI.Label (new Rect((Screen.width  * 0.5f) - 75, (Screen.height * 0.5f + 100), 150, 50), "Press START for menu", style);
 
-			if(Input.GetButtonDown("ToggleSplit")){
+			if(Input.GetButtonDown("Start") || Input.GetKeyDown (KeyCode.Return)){
 				Application.LoadLevel ("title");
 			}
 		}

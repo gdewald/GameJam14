6e90c5ca976f886656954ccc2101e8811050ec77
Isaiah Hines <IsaiahHines@gmail.com>
2014-01-18 22:53:28 -0500

    Enemy Health and Art etc

---
+++ Assets/Scripts/EnemyLarge.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyLarge : Enemy {
+
+	private GameObject enemy = Resources.Load("Enemy") as GameObject;
+	private GameObject firstEnemy;
+	private GameObject secondEnemy;
+
+	public override void TakeHit(Vector2 hitDirection){
+		// React to hit
+		print ("hit large");
+	}
+	
+	public override void TakeChainHit(Vector2 chainDirection, int chainType){
+		switch (chainType) {
+		// Split
+		case 0: 
+		{
+			firstEnemy = Instantiate (enemy, gameObject.transform.position, Quaternion.identity) as GameObject;
+			secondEnemy = Instantiate (enemy, gameObject.transform.position, Quaternion.identity) as GameObject;
+
+			firstEnemy.GetComponent<Enemy>().SetHitsLeft(hitsLeft);
+			secondEnemy.GetComponent<Enemy>().SetHitsLeft(hitsLeft);
+
+			Destroy (gameObject);
+
+			firstEnemy.GetComponent<Follow>().enabled = false;
+			secondEnemy.GetComponent<Follow>().enabled = false;
+			//firstEnemy.transform.rigidbody2D.AddForce(new Vector2(-10000, -10000));
+			//print ("hit");
+			firstEnemy.transform.rigidbody2D.velocity = new Vector2(5, 0);
+			secondEnemy.transform.rigidbody2D.velocity = new Vector2(-5, 0);
+			StartCoroutine(delayedExec());
+			break;
+		}
+		// Die
+		case 1:
+
+			break;
+		}
+	}
+
+	public override void SetHitsLeft (int hitsLeft){
+		this.hitsLeft = hitsLeft;
+		if(hitsLeft > 0)
+			gameObject.GetComponent<SpriteRenderer>().sprite = GameObject.Instantiate(Resources.Load<Sprite>("Images/Enemy" + (hitsLeft-1).ToString())) as Sprite;
+	}
+
+	private IEnumerator delayedExec(){
+		yield return new WaitForSeconds(0.5f);
+		firstEnemy.GetComponent<Follow>().enabled = true;
+		secondEnemy.GetComponent<Follow>().enabled = true;
+	}
+}
---
+++ Assets/Scripts/EnemySmall.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySmall : Enemy {
+	
+	public override void TakeHit(Vector2 hitDirection){
+		SetHitsLeft(hitsLeft-1);
+
+		// Die
+		if (hitsLeft <= 0) {
+			//gameObject.rigidbody.velocity = hitDirection;
+			die ();
+		}
+	}
+	
+	public override void TakeChainHit(Vector2 chainDirection, int chainType){
+		
+	}
+	
+	public override void SetHitsLeft (int hitsLeft){
+		this.hitsLeft = hitsLeft;
+		if(hitsLeft > 0)
+			gameObject.GetComponent<SpriteRenderer>().sprite = GameObject.Instantiate(Resources.Load<Sprite>("Images/Enemy" + (hitsLeft-1).ToString())) as Sprite;
+	}
+
+	void Update(){
+
+	}
+}
